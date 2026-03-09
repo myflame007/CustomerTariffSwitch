a16b7d71fc8c43cb14d1873b94a1634642371cc0
Robert Stickler <robert.stickler@avanade.com>
2026-03-09 13:39:12 +0100

    feat: Created RequestDecision Class

---
+++ CustomerTariffSwitch.Models/RequestDecision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerTariffSwitch.Models
+{
+    public class RequestDecision
+    {
+        public required string RequestId { get; init; }
+        public required string Status { get; init; }
+        public string? Reason { get; init; }
+
+        public static RequestDecision Approved(string requestId) =>
+            new()
+            {
+                RequestId = requestId,
+                Status = "Approved"
+            };
+
+        public static RequestDecision Rejected(string requestId, string reason) =>
+            new()
+            {
+                RequestId = requestId,
+                Status = "Rejected",
+                Reason = reason
+            };
+    }
+}
